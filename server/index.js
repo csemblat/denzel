a74df9d97adee57fe18da12f5b61bf8c2cd9b410
csemblat <csemblat@protonmail.com>
2020-03-24 11:11:32 +0100

    added restful endpoints

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 const {PORT} = require('./constants');
-
+const db_module = require('./test.js')
 const app = express();
-
-module.exports = app;
 
 app.use(require('body-parser').json());
 app.use(cors());
@@ -13,9 +11,57 @@ app.use(helmet());
 
 app.options('*', cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+console.log("you need to run mongodb on localhost port 27017 for this program to work");
+
+app.get('/movies/populate/:id', (req, response) => {
+	const actor = req.params.id;
+	db_module.insert_actor_movies(actor).then(function(result){
+	response.send(result);
+})
 });
+
+app.get('/movies', (req, response) => {
+	db_module.find_mustwatch_movies().then(function(result){
+	response.send(result);
+})
+});
+
+app.get('/movies/:id', (req, response) => {
+	const mov_id = req.params.id;
+	db_module.find_specific_movies(mov_id).then(function(result){
+	response.send(result);
+})
+});
+
+app.get('/movies/search', (req, response) => {
+	console.log(req.query);
+	var lim = 2;
+	var min_meta = 0;
+	if(req.query.hasOwnProperty(limit)){
+		lim = req.query.limit
+	}
+	if(req.query.hasOwnProperty(metascore)){
+		min_meta = req.query.metascore
+	}
+	response.send("hello")
+	db_module.search_movies(lim, min_meta).then(function(result){
+	response.send(result);
+})
+});
+
+
+app.post('/movies/:id', function (req, response) {
+	var review = req.body.review;
+	var review_date = req.body.date;
+	const id = req.params.id;
+	db_module.post_review(id, review, review_date).then(function(result){
+	response.send(result);
+})
+});
+
 
 app.listen(PORT);
 console.log(`📡 Running on port ${PORT}`);
+
+module.exports = app;
+
---
+++ server/test.js
@@ -0,0 +1,122 @@
+const imdb = require('./imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
+const METASCORE = 70;
+const fs = require('fs');
+const {MongoClient} = require('mongodb');
+var assert = require('assert');
+const mongo_url = 'mongodb://127.0.0.1:27017'; //url for local mongo_client
+
+async function start (actor = DENZEL_IMDB_ID, metascore = METASCORE) { //modified version of sandbox.js that writes movies of an actor to a json file
+  try {
+    const movies = await imdb(actor);
+    console.log(JSON.stringify(movies, null, 2));
+	fs.writeFileSync('denzel_movies.json', JSON.stringify(movies) );
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+function random_integer(ceiling)
+{
+	var rand = Math.floor((Math.random() * ceiling));
+	return rand
+}
+
+async function insert_actor_movies(actor){  //populates mongo db with movies that have a specific actor in them
+	const client = new MongoClient(mongo_url ,  { useNewUrlParser: true, useUnifiedTopology: true });
+	try {
+	var mov_list = await imdb(actor);
+	for(let i = 0; i < mov_list.length; i++) //we add the id of the actor in a list of actors that played in the movies for future use
+	{
+		if(mov_list[i].hasOwnProperty("actor"))
+		{
+			mov_list[i]["actor"].push(actor);
+		}
+		else{
+			mov_list[i]["actor"] = [];
+			mov_list[i].actor.push(actor);
+		}
+	}
+    await client.connect();
+	const result = await client.db("movies").collection("inserts").insertMany(mov_list);
+	console.log("inserted : " + result.insertedCount);
+	return { "inserted" :  result.insertedCount};
+	} catch (e) {
+    console.error(e);
+	}
+	 finally {
+        await client.close();
+    }
+
+}
+
+async function find_mustwatch_movies(){ //gives a random must-watch movie
+	const client = new MongoClient(mongo_url ,  { useNewUrlParser: true, useUnifiedTopology: true });
+	try {
+    await client.connect();
+	const cursor = client.db("movies").collection("inserts").find({"metascore" : {$gte : 70}});
+	var result = await cursor.toArray();
+	//console.log(result);
+	var rando_int = random_integer(result.length);
+	//console.log(rando_int);
+	var final_result = result[rando_int];
+	return final_result;
+	} catch (e) {
+    console.error(e);
+	}
+	 finally {
+        await client.close();
+    }
+}
+
+async function find_specific_movies(id){ //gives a specific movie
+	const client = new MongoClient(mongo_url ,  { useNewUrlParser: true, useUnifiedTopology: true });
+	try {
+    await client.connect();
+	const result = await client.db("movies").collection("inserts").findOne({"id" : id})
+	//console.log(result);
+	return result;
+	} catch (e) {
+    console.error(e);
+	}
+	 finally {
+        await client.close();
+    }
+}
+
+async function search_movies(limit, min_metascore){ //search a number of movies (limit being the number of movies) with a metascore greater or equal to
+	const client = new MongoClient(mongo_url ,  { useNewUrlParser: true, useUnifiedTopology: true });
+	try {
+    await client.connect();
+	const cursor = client.db("movies").collection("inserts").find({"metascore" : {$gte : min_metascore}}).sort({"metascore": -1}).limit( limit );
+	var result = await cursor.toArray();
+	console.log("result : " + result[0].metascore);
+	return result;
+	} catch (e) {
+    console.error(e);
+	}
+	 finally {
+        await client.close();
+    }
+}
+
+async function post_review(id, review, review_date){
+	const client = new MongoClient(mongo_url ,  { useNewUrlParser: true, useUnifiedTopology: true });
+	try {
+    await client.connect();
+	var result = await  client.db("movies").collection("inserts").updateOne({ "id" : id }, { $set: {"review" : review , "review_date" : review_date}}, {upsert: true} );
+	console.log(result);
+	return result;
+	} catch (e) {
+    console.error(e);
+	}
+	 finally {
+        await client.close();
+    }
+}
+module.exports = { insert_actor_movies , find_mustwatch_movies , find_specific_movies, search_movies , post_review}
+//start(actor = DENZEL_IMDB_ID, METASCORE);
+search_movies(2, 10);
+//insert_actor_movies(DENZEL_IMDB_ID);
